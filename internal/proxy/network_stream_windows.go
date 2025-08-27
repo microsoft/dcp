2ae50958464a7eb468707b4af4ba23d322db031f
Karol Zadora-Przylecki <karolz@microsoft.com>
2025-08-27 11:24:24 -0700

    Do not report connection reset errors from the proxy (#786)
    In practice connection resets should and are handled exactly the same as ordinary connection close. There is marginal benefit in making the developer aware that the connection ended abruptly, but it almost guaranteed to happen when an Aspire app is run with the IDE (because IDEs tend to kill evenrything forcefully). For this reason reporting these errors is more confusing and unhelpful than otherwise.

---
+++ internal/proxy/network_stream_windows.go
@@ -0,0 +1,18 @@
+//go:build windows
+
+package proxy
+
+import (
+	"errors"
+	"io"
+	"net"
+	"syscall"
+)
+
+// Used to suppress reporting of errors that are expected when the connection is closed.
+func isExpectedConnCloseErr(err error) bool {
+	return errors.Is(err, net.ErrClosed) ||
+		errors.Is(err, io.ErrClosedPipe) ||
+		errors.Is(err, io.EOF) ||
+		errors.Is(err, syscall.WSAECONNRESET)
+}
